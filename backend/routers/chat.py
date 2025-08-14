674117416d6e5416f6726e628ba605d8c121c7b0
Buvindu Suraweera <buvindudsuraweera@gmail.com>
2025-08-14 13:34:43 +0530

    Chatbot added. Includes user first name recognition

--- backend/routers/chat.py
+++ backend/routers/chat.py
@@ -1,5 +1,9 @@
-from fastapi import APIRouter, HTTPException
-from backend.services.chat_service_openai import chat_service
+from fastapi import APIRouter, Depends
+from backend.services.chat_service import chat_service
+from backend.schemas import user
+from backend.utils.token import get_current_user
+from sqlalchemy.orm import Session
+from backend.database.db import get_db
 
 router = APIRouter(
     prefix="/chat",
@@ -7,6 +11,6 @@ router = APIRouter(
 )
 
 @router.post("/")
-async def chat(message: str):
-    reply =  await chat_service(message)
+async def chat(message: str, current_user: user.User = Depends(get_current_user), db: Session = Depends(get_db)):
+    reply =  await chat_service(message, current_user, db)
     return {"reply: ": reply}
