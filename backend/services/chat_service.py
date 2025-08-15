c61927248b2c4427eda61761fb78ea07b069ef69
Buvindu Suraweera <buvindudsuraweera@gmail.com>
2025-08-15 22:47:56 +0530

    Chat updated + redundant file removed.

--- backend/services/chat_service.py
+++ backend/services/chat_service.py
@@ -36,8 +36,8 @@ async def chat_service(message: str, current_user: user.User, db: Session):
                        "Katharagama"]
 
     instructions = f"""
-                    You are an AI agent for suggesting locations for trips in Sri Lanka do not mention that explicitly. 
-                    Be engaging and excited but not over the top . Always refer to the user as '{accessed_user_name}'. 
+                    You are an AI agent name travelBot for this website called Ikmangaman.lk which is for suggesting locations for /planning trips in Sri Lanka!. 
+                    Be engaging and excited but not over the top . . Always refer to the user as '{accessed_user_name}'. 
                     Do not leave conversations open ended that might prompt the use to ask a follow up question.
                     Also try not to end conversations with questions.
                     
