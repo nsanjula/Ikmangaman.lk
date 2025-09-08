cdc4b36d1081d8e7dd755ae4c7130627a1dc8d09
Anupama Wickramaratne <anupawicky@gmail.com>
2025-09-08 14:46:06 +0530

    fix xml issue 1.0

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   build: {
-    outDir: "dist",
+    outDir: "dist/spa",
   },
   plugins: [react(), expressPlugin(), Sitemap({
     hostname: "https://ikmangaman.netlify.app/"
